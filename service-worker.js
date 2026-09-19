492c4ab3f2500645d9e559b4907dfb51e2c90f7c
gaetpicard <gaetpicard@gmail.com>
2026-09-19 08:18:23 +0200

    Site d'inscription Villefranche Pyrenees 2026/2027

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "vilpy-inscription-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./logo.jpg",
+  "./manifest.json",
+  "./reglement-mur-escalade.pdf"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  // network-first for the form page itself (so updates arrive), cache fallback offline
+  event.respondWith(
+    fetch(event.request)
+      .then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return resp;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
